f2cb7d444816f401e2097f1090c38b10f132fbfb
mdrohan <mdsajed210@gmail.com>
2023-04-12 05:21:52 +0600

    loadSpinner

--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -3,14 +3,13 @@ import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import { Outlet, useNavigation } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-// import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 
 const Main = () => {
-  // const navigation = useNavigation();
-  // // console.log(navigation.state);
-  // if (navigation.state === "loading") {
-  //   return <LoadingSpinner />;
-  // }
+  const navigation = useNavigation();
+  if (navigation.state === "loading") {
+    return <LoadingSpinner />;
+  }
   return (
     <div>
       <Header></Header>
